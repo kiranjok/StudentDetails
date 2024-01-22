d79d450ed347dc98e220cd843e00a41acf4cb1de
kiranjot kiranjot <kiranjok@sheridancollege.ca>
2024-01-22 11:02:50 -0500

    version of student details

--- src/studentdetails/StudentDetails.java
+++ src/studentdetails/StudentDetails.java
@@ -15,7 +15,25 @@ public class StudentDetails {
      */
     public static void main(String[] args) {
        
-        Student s1 = new Student();
+       StudentDetails[] list = new StudentDetails[3] ;  //array of object decalration
+      //define 3 students  by creating object of student class
+        var s1 = new StudentDetails(); // s1 - object
+        s1.setName("peter");
+        s1.setAge(21);
+         Student s2 = new Student(); // s2 - object
+        s2.setName("jasdeep");
+        s2.setAge(21);
+         Student s3 = new Student(); // s3 - object
+        s3.setName("kiran");
+        s3.setAge(20);
+        //store object in array
+          list[0] = s1;       
+            list[1] = s2;   
+            list[2] = s2;   
+            for(int i=0;i<list.length;i++) //read the array 
+            {
+                System.out.println(list[i].getName() + " "+list[i].getAge());
+            
     }
     
 }
