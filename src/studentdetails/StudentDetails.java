df3cf532ea0d3553adf1ee5ce5cb4a57ee456bd9
kiranjot kiranjot <kiranjok@sheridancollege.ca>
2024-01-22 10:55:41 -0500

    version of student details

---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author 91826
+ */
+public class StudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Student s1 = new Student();
+    }
+    
+}
